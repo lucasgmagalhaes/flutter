43f9910464793a821498dd117803b0810f34c0ca
Lucas <lucasgsm88@gmail.com>
2019-11-15 21:17:04 -0300

    finished todo list

--- todo_list/lib/main.dart
+++ todo_list/lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 
 import "package:flutter/material.dart";
 import 'package:path_provider/path_provider.dart';
-import 'package:todo_list/todo.model.dart';
 
 void main() {
   runApp(MaterialApp(home: Home()));
@@ -15,7 +14,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<Map<String, dynamic>> _toDoList = [];
+  List _toDoList = [];
+  Map<String, dynamic> _lastRemoved = new Map();
+  int _lastRemovedPos = 0;
   final _todoController = new TextEditingController();
 
   @override
@@ -59,25 +60,81 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.only(top: 10),
-              itemCount: this._toDoList.length,
-              itemBuilder: (context, index) {
-                return CheckboxListTile(
-                  onChanged: (state) {
-                    this.updateTodoTaskState(state, this._toDoList[index]);
-                  },
-                  title: Text(this._toDoList[index].title),
-                  value: this._toDoList[index].done,
-                  secondary: CircleAvatar(
-                    child:
-                        Icon(_toDoList[index].done ? Icons.check : Icons.error),
-                  ),
-                );
-              },
+            child: RefreshIndicator(
+              onRefresh: this._refresh,
+              child: ListView.builder(
+                padding: EdgeInsets.only(top: 10),
+                itemCount: this._toDoList.length,
+                itemBuilder: this.buildItem,
+              ),
             ),
           )
         ],
+      ),
+    );
+  }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      this._toDoList.sort((a, b) {
+        if (a["ok"] && !b["ok"])
+          return 1;
+        else if (!a["ok"] && b["ok"]) return -1;
+        return 0;
+      });
+      this._saveData();
+    });
+  }
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+      onDismissed: (direction) {
+        setState(() {
+          this._lastRemoved = Map.from(this._toDoList[index]);
+          this._lastRemovedPos = index;
+          this._toDoList.removeAt(index);
+          this._saveData();
+
+          final snack = SnackBar(
+              content: Text("Tarefa ${this._lastRemoved["title"]} removida!"),
+              duration: Duration(seconds: 2),
+              action: SnackBarAction(
+                  label: "Desfazer",
+                  onPressed: () {
+                    setState(() {
+                      this
+                          ._toDoList
+                          .insert(this._lastRemovedPos, this._lastRemoved);
+                      this._saveData();
+                    });
+                  }));
+
+          Scaffold.of(context).removeCurrentSnackBar();
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+          color: Colors.red,
+          child: Align(
+            alignment: Alignment(-1.9, 0),
+            child: Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+          )),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        onChanged: (state) {
+          this.updateTodoTaskState(state, this._toDoList[index]);
+        },
+        title: Text(this._toDoList[index]["title"]),
+        value: this._toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
+        ),
       ),
     );
   }
@@ -103,20 +160,19 @@ class _HomeState extends State<Home> {
   }
 
   void _addTodo() {
-    String todo = this._todoController.text;
-    if (todo.isEmpty) {
-      return;
-    }
     setState(() {
-      this._toDoList.add(new Todo(todo, false));
+      Map<String, dynamic> newTodo = new Map();
+      newTodo["title"] = this._todoController.text;
+      newTodo["ok"] = false;
+      this._toDoList.add(newTodo);
       this._todoController.clear();
       this._saveData();
     });
   }
 
-  void updateTodoTaskState(bool state, Todo todo) {
+  void updateTodoTaskState(bool state, dynamic todo) {
     setState(() {
-      todo.done = state;
+      todo["ok"] = state;
     });
   }
 }
